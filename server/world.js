9d2fea9fda3622040cc1d133bfc354f288d7c893
si.llena <sintaevaelena8@gmail.com>
2024-01-21 18:20:09 +0500

    world.js

---
+++ server/world.js
@@ -0,0 +1,26 @@
+const contacts = [
+    {     id: 1,
+          fio: "Стаценко Марина Федоровна",
+          e_mail: "statsenco2502@gmail.com",
+          telephon: "+79123876234",
+    },
+    {
+          id: 2,
+          fio: "Нагиев Дмитрий Петрович",
+          e_mail: "noga@gmail.com",
+          telephon: "+7932178996",
+    },
+    {
+          id: 3,
+          fio: "Тесникова Варавара Васильевна",
+          e_mail: "tesnicovaVarvar12@gmai.com",
+          telephon: "+78652389423",
+    },
+    {
+          id: 4,
+          fio: "Хакимов Артем Максимович",
+          e_mail: "ham33@yandex.ru",
+          telephon: "+78342124598",
+        } 
+    ]
+console.log(contacts[3].id + " - " + contacts[3].fio + " - " + contacts[3].e_mail + " - " + contacts[3].telephon)
