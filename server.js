280e76607d21d178dfd714f9dbb8c96c01261b0f
si.llena <sintaevaelena8@gmail.com>
2024-01-21 18:05:58 +0500

    server.js

--- server.js
+++ server.js
@@ -3,24 +3,24 @@ const app = express()
 const port = 8000
 const contacts = [
     { 
-          id: 3,
+          id: 1,
           fio: "Стаценко Марина Федоровна",
           e_mail: "statsenco2502@gmail.com",
           telephon: "+79123876234",
     },
     {
-          id: 4,
+          id: 2,
           fio: "Нагиев Дмитрий Петрович",
           e_mail: "noga@gmail.com",
           telephon: "+79123876234",
     },
     {
-          id: 5,
+          id: 3,
            fio: "Тесникова Варавара Васильевна",
            e_mail: "tesnicovaVarvar12@gmai.com",
     },
     {
-          id: 6,
+          id: 4,
           fio: "Хакимов Артем Максимович",
           e_mail: "ham33@yandex.ru",
           telephon: "+783421245",
