804402296ae91a69ec12e4c04a8a4dd8873be29a
si.llena <sintaevaelena8@gmail.com>
2024-01-21 17:10:54 +0500

    world.js

--- world.js
+++ world.js
@@ -1,62 +1,40 @@
-const clients = {
-
-      id: 1,
-      fio: "Иванова Мария Николаевна",
-      birthay: "12.09.1990",
-      gender: "ж", 
-
-      id: 2,
-      fio: "Засядько Алена Михайловна",
-      birthay: "01.11.2000",
-      gender: "ж",
-};
-const employees = {
-      id: 3,
-      fio: "Стаценко Марина Федоровна",
-      birthday: "25.01.2001",
-      gender: "Ж",
-  
-      id: 4,
-      fio: "Нагиев почта Петрович",
-      birthday: "23.09.1998",
-      gender: "М",
-
-      id: 5,
-       fio: "Тесникова Варавара Васильевна",
-      birthday: "11.04.1999",
-      gender: "Ж",
-        
-      id: 6,
-      fio: "Хакимов Артем Максимович",
-      birthday: "31.12.1998",
-      gender: "М",   
-  };
-  const contacts = { 
-            id: 3,
-            fio: "Стаценко Марина Федоровна",
-            e_mail: "statsenco2502@gmail.com",
-            telephon: "+79123876234",
-        
-            id: 4,
-            fio: "Нагиев Дмитрий Петрович",
-            e_mail: "noga@gmail.com",
-            telephon: "+79123876234",
-        
-      
-            id: 5,
-             fio: "Тесникова Варавара Васильевна",
-             e_mail: "tesnicovaVarvar12@gmai.com",
-         
-            id: 6,
-            fio: "Хакимов Артем Максимович",
-            e_mail: "ham33@yandex.ru",
-            telephon: "+783421245",
-  };
-
-  
-
-
-console.log(clients.id1+ " - " + clients.fio + " - " + clients.birthay + " - " + clients.gender);
+const express = require("express")
+const app = express()
+const port = 8000
+const contacts = [
+            { 
+                  id: 1,
+                  fio: "Стаценко Марина Федоровна",
+                  e_mail: "statsenco2502@gmail.com",
+                  telephon: "+79123876234",
+            },
+            {
+                  id: 2,
+                  fio: "Нагиев Дмитрий Петрович",
+                  e_mail: "noga@gmail.com",
+                  telephon: "+79123876234",
+            },
+            {
+                  id: 3,
+                   fio: "Тесникова Варавара Васильевна",
+                   e_mail: "tesnicovaVarvar12@gmai.com",
+            },
+            {
+                  id: 4,
+                  fio: "Хакимов Артем Максимович",
+                  e_mail: "ham33@yandex.ru",
+                  telephon: "+783421245",
+            }
+        ];   
+app.get('/', (req, res) => {
+      res.set({
+          "Content-Type": "application/json",
+      })
+      res.json(JSON.stringify(contacts))
+  })
+  app.listen(port, () => {
+      console.log(`Мы начали прослушивать ${port} порт.`)
+  })
 
 
 
