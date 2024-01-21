19461f32ad55c2724d3c960b08673f3d77d0de64
si.llena <sintaevaelena8@gmail.com>
2024-01-21 19:03:24 +0500

    server.js

--- server.js
+++ server.js
@@ -2,30 +2,25 @@ const express = require("express")
 const app = express()
 const port = 8000
 const contacts = [
-{ 
-      id: 1,
-      fio: "Стаценко Марина Федоровна",
-      e_mail: "statsenco2502@gmail.com",
-      telephon: "+79123876234",
-},
-{
-      id: 2,
-      fio: "Нагиев Дмитрий Петрович",
-      e_mail: "noga@gmail.com",
-      telephon: "+79123876234",
-},
-{
-      id: 3,
-      fio: "Тесникова Варавара Васильевна",
-      e_mail: "tesnicovaVarvar12@gmai.com",
-},
-{
-      id: 4,
-      fio: "Хакимов Артем Максимович",
-      e_mail: "ham33@yandex.ru",
-      telephon: "+783421245",
-}
-]
+      {
+          id: 1,
+          fio: "Нагиев Дмитрий Петрович",
+          e_mail: "noga@gmail.com",
+          telephon: "+7932178996",
+      },
+      {
+          id: 2,
+          fio: "Тесникова Варавара Васильевна",
+          e_mail: "tesnicovaVarvar12@gmai.com",
+          telephon: "+78652389423",
+      },
+      {
+          id: 3,
+          fio: "Хакимов Артем Максимович",
+          e_mail: "ham33@yandex.ru",
+          telephon: "+78342124598",
+      } 
+      ]
 app.get('/', (req, res) => {
       res.set({
           "Content-Type": "application/json",
--- world.js
+++ world.js
@@ -1,26 +1,22 @@
 const contacts = [
-    {   id: 1,
-        fio: "Стаценко Марина Федоровна",
-        e_mail: "statsenco2502@gmail.com",
-        telephon: "+79123876234",
-    },
+   
     {
-        id: 2,
+        id: 1,
         fio: "Нагиев Дмитрий Петрович",
         e_mail: "noga@gmail.com",
         telephon: "+7932178996",
     },
     {
-        id: 3,
+        id: 2,
         fio: "Тесникова Варавара Васильевна",
         e_mail: "tesnicovaVarvar12@gmai.com",
         telephon: "+78652389423",
     },
     {
-        id: 4,
+        id: 3,
         fio: "Хакимов Артем Максимович",
         e_mail: "ham33@yandex.ru",
         telephon: "+78342124598",
     } 
     ]
-console.log(contacts[1].id + " - " + contacts[1].fio + " - " + contacts[1].e_mail + " - " + contacts[1].telephon)
+console.log(contacts[2].id + " - " + contacts[2].fio + " - " + contacts[2].e_mail + " - " + contacts[2].telephon)
